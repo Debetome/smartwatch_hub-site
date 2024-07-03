1a067cc151a18f61749912dd4417ad482b313a7a
magregory <derektorresmedina@institutomanuelarevalo.drelm.edu.pe>
2024-07-03 15:22:13 -0500

    Second commit

---
+++ scripts/dummy_data.js
@@ -0,0 +1,89 @@
+const favoriteItems = [
+    {
+        "name": "Samsung Galaxy Watch Active",
+        "price": 679.0,
+        "discount": "32%",
+        "rating": 4.5,
+        "image": "../assets/watch1.png"
+    },
+    {
+        "name": "Samsung Galaxy Watch Active",
+        "price": 679.0,
+        "discount": "32%",
+        "rating": 4.5,
+        "image": "../assets/watch1.png"
+    },
+    {
+        "name": "Samsung Galaxy Watch Active",
+        "price": 679.0,
+        "discount": "32%",
+        "rating": 4.5,
+        "image": "../assets/watch1.png"
+    }
+]
+
+const catalogosItems = [
+    {
+        "name": "Samsung Galaxy Watch Active",
+        "price": 679.0,
+        "discount": "32%",
+        "rating": 4.5,
+        "image": "../assets/watch1.png"
+    },
+    {
+        "name": "Samsung Galaxy Watch Active",
+        "price": 679.0,
+        "discount": "32%",
+        "rating": 4.5,
+        "image": "../assets/watch1.png"
+    },
+    {
+        "name": "Samsung Galaxy Watch Active",
+        "price": 679.0,
+        "discount": "32%",
+        "rating": 4.5,
+        "image": "../assets/watch1.png"
+    },
+    {
+        "name": "Samsung Galaxy Watch Active",
+        "price": 679.0,
+        "discount": "32%",
+        "rating": 4.5,
+        "image": "../assets/watch1.png"
+    },
+    {
+        "name": "Samsung Galaxy Watch Active",
+        "price": 679.0,
+        "discount": "32%",
+        "rating": 4.5,
+        "image": "../assets/watch1.png"
+    },
+    {
+        "name": "Samsung Galaxy Watch Active",
+        "price": 679.0,
+        "discount": "32%",
+        "rating": 4.5,
+        "image": "../assets/watch1.png"
+    },
+    {
+        "name": "Samsung Galaxy Watch Active",
+        "price": 679.0,
+        "discount": "32%",
+        "rating": 4.5,
+        "image": "../assets/watch1.png"
+    },
+    {
+        "name": "Samsung Galaxy Watch Active",
+        "price": 679.0,
+        "discount": "32%",
+        "rating": 4.5,
+        "image": "../assets/watch1.png"
+    },
+    {
+        "name": "Samsung Galaxy Watch Active",
+        "price": 679.0,
+        "discount": "32%",
+        "rating": 4.5,
+        "image": "../assets/watch1.png"
+    }
+]
