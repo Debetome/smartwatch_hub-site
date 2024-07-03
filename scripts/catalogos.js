92c5710cc19d5545bed018666d6bd8b04044bbc5
magregory <magregory@tutanota.com>
2024-07-03 15:56:32 -0500

    Adding neccesary packages and scripts to run github pages

--- scripts/catalogos.js
+++ scripts/catalogos.js
@@ -1,11 +1,21 @@
 const $catalogosList = $('#catalogos-ul-lista');
 
+function displayFavorites() {
+       
+}
+
+function displayCatalogos() {
+
+}
+
 function setGridColumns() {
     const listWidth = $($catalogosList).width();
     const gridColumns = Math.floor(listWidth / 80);
     $catalogosList.css('grid-template-columns', `repeat(${gridColumns}, minmax(250px, 1fr))`);
 }
 
+displayFavorites();
+displayCatalogos();
 setGridColumns();
 
 $(window).resize(function() {
